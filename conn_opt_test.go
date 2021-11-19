c9c77b6bb9be655af1a5870e54930aa2fab012c2
Sam Herrmann <samherrmann.github@gmail.com>
2021-11-19 03:30:03 -0500

    Add ability to set custom logger (#48)
    Before this commit, a custom logger could be set with the LogMessages
    function. However, by using LogMessages not only is a custom logger set
    but also all received and sent messages are logged. Use cases exist
    where a custom logger is desired to log errors but not all messages.

---
+++ conn_opt_test.go
@@ -0,0 +1,53 @@
+package jsonrpc2_test
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"log"
+	"net"
+	"testing"
+
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+func TestSetLogger(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	rd, wr := io.Pipe()
+	defer rd.Close()
+	defer wr.Close()
+
+	buf := bufio.NewReader(rd)
+	logger := log.New(wr, "", log.Lmsgprefix)
+
+	a, b := net.Pipe()
+	connA := jsonrpc2.NewConn(
+		ctx,
+		jsonrpc2.NewBufferedStream(a, jsonrpc2.VSCodeObjectCodec{}),
+		noopHandler{},
+		jsonrpc2.SetLogger(logger),
+	)
+	connB := jsonrpc2.NewConn(
+		ctx,
+		jsonrpc2.NewBufferedStream(b, jsonrpc2.VSCodeObjectCodec{}),
+		noopHandler{},
+	)
+	defer connA.Close()
+	defer connB.Close()
+
+	// Write a response with no corresponding request.
+	if err := connB.Reply(ctx, jsonrpc2.ID{Num: 0}, nil); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "jsonrpc2: ignoring response #0 with no corresponding request\n"
+	got, err := buf.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Fatalf("got %q, want %q", got, want)
+	}
+}
